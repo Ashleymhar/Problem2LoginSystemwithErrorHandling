37735d5ecb81f371f4ef2ba2af6faa71eb2e9dd2
AshleymharBautista <AshleymharBautista@SMU-0111>
2025-09-12 09:22:08 +0800

    Add project files.

---
+++ Problem2LoginSystemwithErrorHandling/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Username = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Username
+        '
+        Me.Username.AutoSize = True
+        Me.Username.Location = New System.Drawing.Point(230, 9)
+        Me.Username.Name = "Username"
+        Me.Username.Size = New System.Drawing.Size(55, 13)
+        Me.Username.TabIndex = 0
+        Me.Username.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(232, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(305, 6)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsername.TabIndex = 2
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(305, 32)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(100, 20)
+        Me.txtPassword.TabIndex = 3
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(316, 58)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(334, 84)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(37, 13)
+        Me.lblResult.TabIndex = 5
+        Me.lblResult.Text = "Result"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Username)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Username As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents lblResult As Label
+End Class
---
+++ Problem2LoginSystemwithErrorHandling/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+        Try
+            Dim username As String = txtUsername.Text.Trim()
+            Dim password As String = txtPassword.Text.Trim()
+
+            If String.IsNullOrEmpty(username) Then
+                Throw New LoginFieldEmptyException("Username cannot be empty.")
+            End If
+
+            If String.IsNullOrEmpty(password) Then
+                Throw New LoginFieldEmptyException("Password cannot be empty.")
+            End If
+            lblResult.Text = $"Welcome, {username}!"
+
+        Catch ex As LoginFieldEmptyException
+            lblResult.Text = ex.Message
+        End Try
+    End Sub
+
+End Class
+
+' 🔐 Custom Exception for Login System
+Public Class LoginFieldEmptyException
+    Inherits Exception
+
+    Public Sub New(message As String)
+        MyBase.New(message)
+    End Sub
+
+End Class
